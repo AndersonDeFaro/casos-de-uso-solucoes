50a9b9dec46979d2b2572a86053afcc5c625e7fd
andersondefaro <anderson_faro@hotmail.com>
2025-09-24 08:59:09 -0300

    Ajustando router de Endpoint

--- 8 - FastAPI/api_dados_abertos/app/api/endpoints/router.py
+++ 8 - FastAPI/api_dados_abertos/app/api/endpoints/router.py
@@ -1,17 +1,15 @@
 from fastapi import APIRouter
 # Importe os routers do subdiretório 'system_a'
-from .system_a.users import router as users_router
-from .system_a.products import router as products_router
+from .system_a.router import router as system_a_router
 
 # Importe o router do subdiretório 'deputado_federal'
-from .deputado_federal.estado import router as deputado_federal_router
+from .deputado_federal.router import router as deputado_federal_router
 
 # Cria a instância principal do roteador para este módulo
 api_router = APIRouter()
 
 # Inclui os roteadores, especificando seus prefixos de URL e tags
-api_router.include_router(users_router, prefix="/users", tags=["Users"])
-api_router.include_router(products_router, prefix="/products", tags=["Products"])
+api_router.include_router(system_a_router)
 
 # Exemplo de como incluir o router para deputado_federal
-api_router.include_router(deputado_federal_router, prefix="/deputado_federal", tags=["Deputados Federais"])
+api_router.include_router(deputado_federal_router)
